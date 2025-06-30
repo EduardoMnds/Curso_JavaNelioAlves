7b4979a9c58dd9d41fb52446582ae29bd7fa5f79
EduardoMnds <eduardo2002brasil@gmail.com>
2025-06-30 17:59:48 -0300

    Conceiro de Herança

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
     }
 }
