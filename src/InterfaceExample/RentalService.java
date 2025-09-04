02c3d953b64e996dce797a948fc17b63a41f9152
EduardoMnds <eduardo2002brasil@gmail.com>
2025-09-04 20:52:19 -0300

    Exemplo utilizando o conceito de Interface

---
+++ src/InterfaceExample/RentalService.java
@@ -0,0 +1,32 @@
+package InterfaceExample;
+
+import java.time.Duration;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+    private TaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour , TaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+        double Minutes = Duration.between(carRental.getStart(), carRental.getEnd()).toMinutes();
+        double hours = Minutes / 60;
+
+        double basicPayment;
+        if (hours <= 12) {
+            basicPayment = pricePerHour * Math.ceil(hours);
+        } else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24);
+        }
+        double tax = taxService.tax(basicPayment);
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+
+}
